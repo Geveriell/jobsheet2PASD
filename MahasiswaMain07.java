509894592c1a4a7a1dba88ed1e873e0ce4e3a7c9
Geveriell <erzaardhyaputra@gmail.com>
2026-02-18 11:23:06 +0700

    2.3.1.2 Langkah-langkah Percobaan

--- MahasiswaMain07.java
+++ MahasiswaMain07.java
@@ -1,5 +1,6 @@
 public class MahasiswaMain07 {
-     public static void main(String[] args) {
+
+    public static void main(String[] args) {
 
         mahasiswa07 mhs1 = new mahasiswa07();
 
@@ -9,12 +10,16 @@ public class MahasiswaMain07 {
         mhs1.ipk = 3.55;
 
         mhs1.tampilkanInformasi();
-
-        System.out.println("\nSetelah perubahan:");
-
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIpk(3.60);
-
         mhs1.tampilkanInformasi();
+
+        System.out.println();
+
+        mahasiswa07 mhs2 = new mahasiswa07 ("2141720160", "Annisa Nabila", "TI 2L", 3.25);
+
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
+
